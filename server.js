60a27fd8033c45b2dd46b2cd622805afd0e1aa94
Claire <claire.3.eberle@gmail.com>
2023-02-01 09:53:42 -0800

    added note id and post request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Notes = require ('./db/db.json');
+const path = require('path');
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
@@ -10,13 +12,38 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'))
-})
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req,res) => {
+    res.json(Notes)
+});
+
+app.post('/api/notes', (req,res) => {
+console.info(`${req.method} reqeust recieved to add a notes`);
+
+const {title, noteText } = req.body;
+if(title && noteText ) {
+    const newNote = {
+        title,
+        newNote,
+        note_id: uuid(),
+    };
+    const response = {
+        status: 'success!',
+        body: newNote,
+    }
+    console.log(response);
+res.status(201).json(response);
+} else {
+    res.status(500).json('Error in posting new note')
+}
+})
 
 app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`)
